de32aa1b3d90b2fa5a211739e619640bde2683aa
OliLobster <sgyr1993@163.com>
2022-09-18 20:33:57 +0800

    fix compile error on svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import starlinkLogo  from '../assets/images/Starlink_Logo.svg';
+import starlinkLogo  from '../assets/images/starlink_logo.svg';
 
 class Header extends Component {
     render() {
